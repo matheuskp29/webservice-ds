42fe895045d213fed3e504bce38c1b3552d7f985
Matheus Kaczan <matheuskp29@gmail.com>
2022-02-09 13:18:47 -0300

    Project created

---
+++ src/test/java/com/projectservice/webservicedev/WebservicedevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectservice.webservicedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicedevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
